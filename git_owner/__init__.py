9c20189ab58ab32b8985668f6787ba772a0b5f91
Marek Suchánek <msuchane@redhat.com>
2023-10-24 19:27:59 +0200

    Log and blame authors

--- git_owner/__init__.py
+++ git_owner/__init__.py
@@ -1,11 +1,29 @@
+import subprocess
 import sys
-
-from git import Repo
 
 file = sys.argv[1]
 
-repo = Repo(".")
+def log_contributors(file: str) -> list[str]:
+    log = subprocess.run(["git", "log", "--follow", "--format=%ae", file], stdout=subprocess.PIPE)
+    contributors = log.stdout.decode().splitlines()
 
-blame = repo.blame(repo.head, file)
+    return contributors
 
-print(blame)
+print(log_contributors(file))
+
+
+def blame_contributors(file: str) -> list[str]:
+    blame_contributors = []
+
+    blame = subprocess.run(["git", "blame", "--line-porcelain", file], stdout=subprocess.PIPE)
+    blame_text = blame.stdout.decode().splitlines()
+
+    for index, line in enumerate(blame_text):
+        if index % 13 == 2:
+            assert line.startswith("author-mail")
+            author = line[13:-1]
+            blame_contributors.append(author)
+
+    return blame_contributors
+
+print(blame_contributors(file))
