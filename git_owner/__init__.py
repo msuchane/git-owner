ebf097ec428923514ba1f654107a1c4b3efc9a38
Marek Suchánek <msuchane@redhat.com>
2023-10-24 19:45:39 +0200

    Calculate contributor fractions

--- git_owner/__init__.py
+++ git_owner/__init__.py
@@ -1,15 +1,13 @@
+from collections import Counter
 import subprocess
 import sys
 
-file = sys.argv[1]
 
 def log_contributors(file: str) -> list[str]:
     log = subprocess.run(["git", "log", "--follow", "--format=%ae", file], stdout=subprocess.PIPE)
     contributors = log.stdout.decode().splitlines()
 
     return contributors
-
-print(log_contributors(file))
 
 
 def blame_contributors(file: str) -> list[str]:
@@ -26,4 +24,28 @@ def blame_contributors(file: str) -> list[str]:
 
     return blame_contributors
 
-print(blame_contributors(file))
+
+def contributor_shares(authors: list[str]) -> dict:
+    counter = Counter(authors)
+    total = counter.total()
+
+    shares = {}
+
+    for author, count in counter.most_common():
+        fraction = count / total
+        shares[author] = fraction
+
+    return shares
+
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+
+    from_log = log_contributors(file)
+    log_shares = contributor_shares(from_log)
+
+    from_blame = blame_contributors(file)
+    blame_shares = contributor_shares(from_blame)
+
+    print("log:", log_shares)
+    print("blame:", blame_shares)
