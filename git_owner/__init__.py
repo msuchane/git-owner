89fcd397197210e78e9edfc765d6aa1c6291dfc9
Marek Suchánek <msuchane@redhat.com>
2023-11-08 17:06:34 +0100

    Enable the placeholder owner

--- git_owner/__init__.py
+++ git_owner/__init__.py
@@ -166,7 +166,7 @@ def estimate_file(file: str, args: argparse.Namespace) -> SortedShares:
         try:
             log_shares = contributor_shares(buffer["log"])
         except KeyError:
-            sys.exit(1)
+            exit_no_owner(args)
 
         logging.debug(f"Contributors:\n{log_shares}")
         sorted_shares = sort_shares(log_shares)
@@ -177,7 +177,7 @@ def estimate_file(file: str, args: argparse.Namespace) -> SortedShares:
         try:
             blame_shares = contributor_shares(buffer["blame"])
         except KeyError:
-            sys.exit(1)
+            exit_no_owner(args)
 
         logging.debug(f"Contributors:\n{blame_shares}")
         sorted_shares = sort_shares(blame_shares)
@@ -197,7 +197,7 @@ def estimate_file(file: str, args: argparse.Namespace) -> SortedShares:
             log = buffer["log"]
             blame = buffer["blame"]
         except KeyError:
-            sys.exit(1)
+            exit_no_owner(args)
 
         log_shares = contributor_shares(log)
         blame_shares = contributor_shares(blame)
@@ -228,6 +228,27 @@ def print_report(shares: SortedShares, file: str, args: argparse.Namespace) -> N
         else:
             header = None
         report_shares(shares, header)
+
+
+def exit_no_owner(args: argparse.Namespace) -> None:
+    if args.placeholder is None:
+        sys.exit(1)
+
+    if args.most_likely:
+        print(args.placeholder)
+        sys.exit(1)
+        
+    if len(args.files) > 1:
+        header = file
+    else:
+        header = None
+
+    # The placeholder owner pretends to own the whole failed file.
+    placeholder_shares = [(args.placeholder, 1.0)]
+
+    report_shares(placeholder_shares, header)
+
+    sys.exit(1)
 
 
 if __name__ == "__main__":
